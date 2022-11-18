3207c0b15c6f093597aa603a18e67fac75b21e6b
ALBERT14143 <catorsi.albert14@gmail.com>
2022-11-18 09:18:56 +0800

    creating bloc

---
+++ lib/provider/bloc_provider.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/cupertino.dart';
+import 'package:test_app2/provider/bloc/test_bloc.dart';
+
+class BlocProvider extends InheritedWidget {
+  final TestBloc testBloc;
+
+  const BlocProvider({
+    required this.testBloc,
+    required Widget child,
+    Key? key
+  }) : super(child: child, key: key);
+
+  static BlocProvider? of(BuildContext context) => 
+    context.dependOnInheritedWidgetOfExactType<BlocProvider>();
+  
+  @override
+  bool updateShouldNotify(covariant InheritedWidget oldWidget) => true;
+
+}
